a61681abbdb8d155278b2a8fb0bd27df981e6152
Bill G <gbilly3@gmail.com>
2019-10-24 18:36:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code here
+
+
+function saturdayFun(activity="roller-skate") {
+return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork (x = "go to the office"){
+
+  return `This Monday, I will ${x}.`
+}
+
+function wrapAdjective (abc = "*"){
+
+
+
+
+
+}
